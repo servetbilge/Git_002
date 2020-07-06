ae10fa96cd890bcd750963f044aa40b7a95ef5a3
servetbilge <67020144+servetbilge@users.noreply.github.com>
2020-07-06 20:37:43 +0200

    Update Class1.java
    değişim masterdan oldu

--- src/Class1.java
+++ src/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
 		
 		System.out.println("Local master branch");
 		System.out.println("Local dan ikinci commit");
+		System.out.println("Masterdan 1.");
+		System.out.println("Masterdan 2.");
 
 	}
 
